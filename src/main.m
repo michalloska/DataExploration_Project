ee57d7dc33e43f5b5c82256227dd9c3a5edd8eb9
michalloska <root@N-20L6PF15HU2D.nsn-intra.net>
2020-11-21 12:41:14 +0100

    Added Histogram Titles and axis

--- src/main.m
+++ src/main.m
@@ -15,11 +15,23 @@ allDieselAutomaticCars = GetCarsByFuelType('Diesel', allAutomaticCars);
 allPetrolManualCars = GetCarsByFuelType('Petrol', allManualCars);
 allDieselManualCars = GetCarsByFuelType('Diesel', allManualCars);
 
+
 histogram(allPetrolAutomaticCars.engine_capacity, [50:100:5000]);
+title('all petrol automatic cars');
+xlabel('Engine Capcity [L]');
+ylabel('Amount of Cars');
 figure
 histogram(allDieselAutomaticCars.engine_capacity, [50:100:5000]);
+title('all diesel automatic cars');
+xlabel('Engine Capcity [L]');
+ylabel('Amount of Cars');
 figure
 histogram(allPetrolManualCars.engine_capacity, [50:100:5000]);
+title('all petrol manual cars');
+xlabel('Engine Capcity [L]');
+ylabel('Amount of Cars');
 figure
 histogram(allDieselManualCars.engine_capacity, [50:100:5000]);
-
+title('all diesel manual cars');
+xlabel('Engine Capcity [L]');
+ylabel('Amount of Cars');
